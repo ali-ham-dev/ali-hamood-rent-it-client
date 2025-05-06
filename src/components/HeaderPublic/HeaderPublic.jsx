c4680a53826230cbe7ffb49212173259430e04d4
Ali Hamood <ali.hamood@gov.bc.ca>
2025-05-06 10:06:16 -0700

    Adds font sizes and sample font displaies. Adds logo component to heaer.

---
+++ src/components/HeaderPublic/HeaderPublic.jsx
@@ -0,0 +1,14 @@
+import './HeaderPublic.scss';
+import Logo from '../Logo/Logo';
+
+const HeaderPublic = () => {
+    return (
+        <header>
+            <Logo />
+            <p>Rent it</p>
+        </header>        
+    )
+}
+
+export default HeaderPublic;
+
