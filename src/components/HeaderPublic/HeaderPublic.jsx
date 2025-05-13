cbf0362db15f9b120ca0b2b7f8b0bed628e2418b
Ali Hamood <ali.hamood@gov.bc.ca>
2025-05-12 22:16:24 -0700

    Adds mobile verison

--- src/components/HeaderPublic/HeaderPublic.jsx
+++ src/components/HeaderPublic/HeaderPublic.jsx
@@ -6,8 +6,12 @@ const HeaderPublic = () => {
     return (
         <header className='header-public'>
             <Logo />
+            <div className='header-public__buttons header-public__buttons-mobile'>
+                <button className='header-public__button'>Login</button>
+                <button className='header-public__button'>Sign Up</button>
+            </div>
             <Search />
-            <div className='header-public__buttons'>
+            <div className='header-public__buttons header-public__buttons-desktop'>
                 <button className='header-public__button'>Login</button>
                 <button className='header-public__button'>Sign Up</button>
             </div>
