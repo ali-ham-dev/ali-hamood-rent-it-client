e8c7da393953abe0e2e0b593df96960a3d5bd15f
Ali Hamood <ali.hamood@gov.bc.ca>
2025-05-17 13:30:58 -0700

    Adds mixin for box shadow. Update font mixing to handle mobile transition. Adds more style to product card.

--- src/components/ProductCardPublic/ProductCardPublic.jsx
+++ src/components/ProductCardPublic/ProductCardPublic.jsx
@@ -15,21 +15,69 @@ const ProductCardPublic = ({}) => {
     // Product charge period
     // is active
 
+    // TODO: get product id from url.
     const productId = 1;
     const apiUrl = import.meta.env.VITE_API_URL;
 
+    const getMediaType = (url) => {
+        if (!url) 
+            return null;
+
+        // TODO: backend should return the media type.
+        const imageTypes = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'webp'];
+        const videoTypes = ['mp4', 'webm', 'ogg', 'mov', 'avi', 'mkv'];
+
+        const fileExtension = url.split('.').pop().toLowerCase();
+
+        if (imageTypes.includes(fileExtension)) 
+            return 'image';
+        if (videoTypes.includes(fileExtension)) 
+            return 'video';
+
+        return null;
+    }
+
+    // 
+    const renderMedia = (url, className = '') => {
+        const mediaType = getMediaType(url);
+
+        if (mediaType === 'image') {
+            return <img 
+                src={url} 
+                alt={''} 
+                className={className}
+            />;
+        }
+
+        if (mediaType === 'video') {
+            return <video 
+                src={url} 
+                controls
+                className={className}
+            />;
+        }
+
+        return <img 
+            src={'/media/images/place_holder.png'} 
+            alt={'placeholder image'} 
+            className={className}
+        />;
+    }
+
     const [product, setProduct] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [media, setMedia] = useState(null);
+    const [mediaIndex, setMediaIndex] = useState(0);
 
     useEffect(()=>{
         const fetchProduct = async () => {
             try {
                 const response = await axios.get(`${apiUrl}/products/${productId}`);
-                setProduct(response.data);
-                setLoading(false);
-                console.log(response.data);
-                console.log(`${apiUrl}/products/${productId}`);
-
+                setTimeout(() => {
+                    setProduct(response.data);
+                    setMedia(JSON.parse(response.data.media));
+                    setLoading(false);
+                }, 2000);
             } catch (error) {
                 console.error(`Error fetching product with id: ${productId}`, error);
             }
@@ -37,19 +85,23 @@ const ProductCardPublic = ({}) => {
         fetchProduct();
     }, [])
 
-    if (loading) {
-        return (
-            <div>Loading...</div>
-        );
-    }
-
     return (
         <div className='product-card-public'>
-            <picture className='product-card-public__picture'>
-                {/* <img src={product.image} alt={product.name} /> */}
-            </picture>
+            <section className="product-card-public__gallary">
+                {/* <button className="product-card-public__button"></button> */}
+                {loading ? (
+                    <img
+                        src={'/media/images/place_holder.png'}
+                        alt="placeholder image"
+                        className="product-card-public__image"
+                    />
+                ) : (
+                    renderMedia(media[mediaIndex], 'product-card-public__image')
+                )}
+                {/* <button className="product-card-public__button"></button> */}
+            </section>
             <section className="product-card-public__summary">
-                <h2 className="product-card-public__title">Product Name</h2>
+                <h3 className="product-card-public__title">Product Name</h3>
                 <p className="product-card-public__description">Product Description</p>
                 <p className="product-card-public__price">Product Price</p>
             </section>
