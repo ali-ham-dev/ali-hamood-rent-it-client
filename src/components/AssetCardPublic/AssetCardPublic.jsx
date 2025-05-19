bf9cb229229f57c9709210169994326216525372
Ali Hamood <ali.hamood@gov.bc.ca>
2025-05-19 15:51:55 -0700

    Minor changes for backend data structure.

--- src/components/AssetCardPublic/AssetCardPublic.jsx
+++ src/components/AssetCardPublic/AssetCardPublic.jsx
@@ -13,6 +13,8 @@ const AssetCardPublic = ({assetId}) => {
     const [imageExtensions, setImageExtensions] = useState(null);
     const [videoExtensions, setVideoExtensions] = useState(null);
 
+    // Helper functions
+
     const getMediaType = (url) => {
         if (!url) 
             return null;
@@ -27,8 +29,19 @@ const AssetCardPublic = ({assetId}) => {
         return null;
     }
 
+    const noImageFound = (className = '') => {
+        return <img 
+            src={'/media/images/place_holder.png'} 
+            alt={'placeholder image'} 
+            className={className}
+        />;
+    }
+
     const renderMedia = (url, className = '') => {
         const mediaType = getMediaType(url);
+
+        if (loading || mediaType === null)
+            return noImageFound(className);
 
         if (mediaType === 'image') {
             return <img 
@@ -46,20 +59,21 @@ const AssetCardPublic = ({assetId}) => {
             />;
         }
 
-        return <img 
-            src={'/media/images/place_holder.png'} 
-            alt={'placeholder image'} 
-            className={className}
-        />;
+        return noImageFound(className);
     }
+
+    // Loading component data.
 
     const fetchAssetDetails = async () => {
         const response = await axios.get(`${apiUrl}/assets/${assetId}`);
 
         if (response.status === 200) {
             setAsset(response.data);
-            setMedia(JSON.parse(response.data.media));
-            setLoading(false);
+            console.log(response.data);
+            setMedia([
+                ...(response.data.media.images || []), 
+                ...(response.data.media.videos || [])
+            ]);
         }
     }
 
@@ -79,19 +93,21 @@ const AssetCardPublic = ({assetId}) => {
         }
     }
 
-
-
     useEffect(()=>{
         const fetchAsset = async () => {
             try {
-                await fetchFileExtensions();
                 await fetchAssetDetails();
+                await fetchFileExtensions();
+                setLoading(false);
             } catch (error) {
                 console.error(`Error fetching asset with id: ${assetId}`, error);
+                setLoading(true);
             }
         }
         fetchAsset();
     }, []);
+
+    // Event Handlers
 
     const handleLeftButtonClick = () => {
         const nextMediaIndex = mediaIndex - 1;
@@ -124,15 +140,7 @@ const AssetCardPublic = ({assetId}) => {
                             alt="arrow left" 
                             className="asset-card-public__button-icon"/>
                 </button>
-                {loading ? (
-                    <img
-                        src={'/media/images/place_holder.png'}
-                        alt="placeholder image"
-                        className="asset-card-public__image"
-                    />
-                ) : (
-                    renderMedia(media[mediaIndex], 'asset-card-public__image')
-                )}
+                {renderMedia(media[mediaIndex], 'asset-card-public__image')}
                 <button 
                     className="asset-card-public__button asset-card-public__button-right"
                     onClick={handleRightButtonClick}>
@@ -145,14 +153,14 @@ const AssetCardPublic = ({assetId}) => {
             <section className="asset-card-public__summary">
                 <Link to={`/products/${assetId}`} className="asset-card-public__title-link">
                     <h3 className="asset-card-public__title">
-                        {loading ? 'loading...' : asset.title}
+                        {loading ? 'loading...' : asset.asset.title}
                     </h3>
                 </Link>
                 <p className="asset-card-public__description">
-                    {loading ? 'loading...' : asset.description}
+                    {loading ? 'loading...' : asset.asset.description}
                 </p>
                 <p className="asset-card-public__price">
-                    {loading ? 'loading...' : `$${asset.cost} per ${asset.charge_frequency}`}
+                    {loading ? 'loading...' : `$${asset.asset.billing.price} / ${asset.asset.billing.billing_frequency}`}
                 </p>
             </section>
         </div>
