4e36db822d9c7ff6b9f5d930ca2e0b8c4e67b2e9
Ali Hamood <ali.hamood@gov.bc.ca>
2025-05-29 18:29:46 -0700

    Changes button location in mobile.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,8 @@ const Header = () => {
     return (
         <header className='header'>
             <Logo />
-            <div className='header__buttons header__buttons-mobile'>
-                <button className='header__button'>Login</button>
-                <button className='header__button'>Sign Up</button>
-            </div>
             <Search />
-            <div className='header__buttons header__buttons-desktop'>
+            <div className='header__buttons'>
                 <button className='header__button'>Login</button>
                 <button className='header__button'>Sign Up</button>
             </div>
