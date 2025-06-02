0855165e85515a52f44496fff8cdab2c85a76f68
Ali Hamood <ali.hamood@gov.bc.ca>
2025-06-01 17:18:14 -0700

    Finishes login page.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './Login.scss';
 import InputBox from '../../components/InputBox/InputBox';
@@ -10,6 +10,7 @@ const loginWithEmailEp = import.meta.env.VITE_LOGIN_WITH_EMAIL_EP;
 
 const Login = () => {
 
+    const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
     const [email, setEmail] = useState({
@@ -29,6 +30,12 @@ const Login = () => {
         const value = inputField.value;
         inputField.error = false;
 
+        if (value.length === 0) {
+            inputField.error = true;
+            inputField.errorMessage = 'Email is required';
+            return;
+        }
+
         if (value.length > 254) {
             inputField.error = true;
             inputField.errorMessage = 'Email must be less than 254 characters';
@@ -47,12 +54,13 @@ const Login = () => {
             const response = await axios.post(`${apiUrl}${checkEmailEp}`, {
                 email: value
             });
+
             if (response &&
                 response.status === 200 &&
-                response.data.emailIsAvailable === false
+                response.data.emailIsAvailable === true
             ) {
                     inputField.error = true;
-                    inputField.errorMessage = 'Email already exists';
+                    inputField.errorMessage = 'Email does not exist';
                     return;
             }
         } catch (error) {
@@ -66,13 +74,49 @@ const Login = () => {
         validateEmailOnBlur(email);
     }
 
+    const handleEmailChange = (e) => {
+        const newEmail = {
+            ...email,
+            value: e.target.value
+        }
+        setEmail(newEmail);
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsLoading(true);
+        setError('');
         
+        await validateEmailOnBlur(email);
+        if (email.error) {
+            setError(email.errorMessage);
+            setIsLoading(email.false);
+            return;
+        }
+
         try {
-
+            const response = await axios.post(`${apiUrl}${loginWithEmailEp}`, {
+                email: email.value
+            });
+            console.log(response);
+            if (response.status === 200) {
+                console.log('response.data');
+                navigate('/user-auth' +
+                    '/' +
+                    encodeURIComponent(response.data.user.id).toString() +
+                    '/' +
+                    encodeURIComponent(email.value).toString() +
+                    '/' +
+                    encodeURIComponent(response.data.token.expires).toString());
+            }
         } catch (error) {
+            console.error('Error logging in', error);
 
+            if (error?.response?.status === 400) {
+                setError(`Please enter the correct email: ${error.response?.data?.error}`);
+            } else {
+                setError('An error occurred while logging in.');
+            }
         } finally {
             setIsLoading(false);
         }
@@ -88,7 +132,7 @@ const Login = () => {
                     </div>
                 )}
                 <form className="login__form" onSubmit={handleSubmit}>
-                    <InputBox inputBoxData={email} onBlur={handleEmailBlur} />
+                    <InputBox inputBoxData={email} onBlur={handleEmailBlur} onChange={handleEmailChange} />
                     <div className="login__buttons">
                         <button
                             type="submit"
