d72f24765e747897de0a4e287e42fedc26e23c1b
Ali Hamood <ali.hamood@gov.bc.ca>
2025-06-04 15:08:32 -0700

    Adds drap and drop preview.

--- src/components/MediaUploadBox/MediaUploadBox.jsx
+++ src/components/MediaUploadBox/MediaUploadBox.jsx
@@ -10,10 +10,10 @@ const imageExtensionsEp = import.meta.env.VITE_IMG_FILE_EX_EP;
 const videoExtensionsEp = import.meta.env.VITE_VID_FILE_EX_EP;
 
 const MediaUploadBox = () => {
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const [imageExtensions, setImageExtensions] = useState([]);
     const [videoExtensions, setVideoExtensions] = useState([]);
-
+    const [files, setFiles] = useState([]);
 
     const fetchFileExtensions = async () => {
         try {
@@ -22,8 +22,6 @@ const MediaUploadBox = () => {
                 const ext = imageResponse.data.map(item => item.extension);
                 const formattedExt = ext.map(item => item.startsWith('.') ? item : `.${item}`);
                 setImageExtensions(formattedExt);
-                console.log('image extensions');
-                console.log(formattedExt); 
             }
     
             const videoResponse = await axios.get(`${apiUrl}${videoExtensionsEp}`);
@@ -31,12 +29,7 @@ const MediaUploadBox = () => {
                 const ext = videoResponse.data.map(item => item.extension);
                 const formattedExt = ext.map(item => item.startsWith('.') ? item : `.${item}`);
                 setVideoExtensions(formattedExt);
-                console.log('video extensions');
-                console.log(formattedExt); 
             }
-            console.log('------');
-            console.log(imageExtensions);
-            console.log(videoExtensions);
         } catch (error) {
             console.error('Error fetching file extensions:', error);
         }
@@ -44,15 +37,17 @@ const MediaUploadBox = () => {
 
     useEffect(() => {
         fetchFileExtensions();
-    }, []);
-    
-    useEffect(() => {
         setIsLoading(false);
-    }, [imageExtensions, videoExtensions]);
-
-    const onDrop = useCallback((acceptedFiles) => {
-        console.log(acceptedFiles);
     }, []);
+
+    const onDrop = useCallback((dripedFiles) => {
+        const newFiles = dripedFiles.map(file => {
+            file.preview = URL.createObjectURL(file)
+            return file;
+        });
+
+        setFiles(prevFiles => [...prevFiles, ...newFiles]);
+    }, [files]);
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         onDrop: onDrop,
@@ -72,11 +67,22 @@ const MediaUploadBox = () => {
             ) : (                
                 <div className='media-upload-box__dropzone' {...getRootProps()}>
                     <input {...getInputProps()} />
-                    {isDragActive ? (
-                        <p className='media-upload-box__dropzone-text'>Drop your photos and videos here...</p>
-                    ) : (
-                        <p className='media-upload-box__dropzone-text'>Drag and drop images and/or videos here, or click to select files</p>
-                    )}
+                    <p className='media-upload-box__dropzone-text'>Drag and drop your photos and videos here...</p>
+                    <p className='media-upload-box__dropzone-text'>Maximum 10 files, 100MB each</p>
+
+                </div>
+            )}
+            {files.length > 0 && (
+                <div className='media-upload-box__preview'>
+                    {files.map((file, index) => (
+                        <div key={index} className='media-upload-box__preview-item'>
+                            {file.type.startsWith('image/') ? (
+                                <img className='media-upload-box__preview-image' src={file.preview} alt={file.name} />
+                            ) : (
+                                <video className='media-upload-box__preview-video' src={file.preview} controls />
+                            )}
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
