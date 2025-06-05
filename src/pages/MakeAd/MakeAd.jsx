4bf1f48df1d179ab69ea69cbae6e3fed36c91d27
Ali Hamood <ali.hamood@gov.bc.ca>
2025-06-05 08:43:05 -0700

    Adds upload method.

--- src/pages/MakeAd/MakeAd.jsx
+++ src/pages/MakeAd/MakeAd.jsx
@@ -10,6 +10,7 @@ import MediaUploadBox from '../../components/MediaUploadBox/MediaUploadBox';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 const tinymceEp = import.meta.env.VITE_TINYMCE_EP;
+const uploadAssetEp = import.meta.env.VITE_ASSET_UPLOAD_EP;
 
 const MakeAd = ({ jwt }) => {
     const navigate = useNavigate();
@@ -18,6 +19,9 @@ const MakeAd = ({ jwt }) => {
 
     const [isLoading, setIsLoading] = useState(true);
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
     const [titleInputBox, setTitleInputBox] = useState({
         htmlFor: 'ad',
         labelText: '',
@@ -42,13 +46,14 @@ const MakeAd = ({ jwt }) => {
         isRequired: true,
         maxLength: 1000
     });
-    const [period, setPeriod] = useState('Select Charge Period');
+    const [period, setPeriod] = useState('month');
     const [content, setContent] = useState([
         'day',
         'week',
         'month',
         'year'
     ]);
+    const [description, setDescription] = useState('');
     const [uploadMedia, setUploadMedia] = useState(false);
 
     const fetchTinymceSessionJwt = async() => {
@@ -107,14 +112,14 @@ const MakeAd = ({ jwt }) => {
 
     useEffect( () => {
         // TOKEN based auth is a paid tier.
-        // TODO: self host tinymce or switch to prosemirror. 
-        // For now, we're using the api key.
+        // TODO: self host tinymce or switch to prosemirror. For now, we're using the api key.
         fetchTinymceApiKey();
         setIsLoading(false);
     }, [jwt]);
 
     const handleEditorChange = (content, editor) => {
         console.log('TinyMCE Content:', content);
+        setDescription(content);
     };
 
     const validateTitleChange = (input) => {
@@ -131,6 +136,23 @@ const MakeAd = ({ jwt }) => {
         }
     }
 
+    const validatePriceChange = (input) => {
+        if (input.length > 10000) {
+            priceInputBox.error = true;
+            priceInputBox.errorMessage = 'Price must be less than 1000 characters.';
+            return false;
+        }
+
+        return true
+    }
+
+    const validatePriceBlur = (input) => {
+        if (!input || input.length < 1) {
+            priceInputBox.error = true;
+            priceInputBox.errorMessage = 'Price is required.';
+        }
+    }
+
     const handleInputBoxChange = (e) => {
         if (e.target.name === 'title') {
             titleInputBox.error = false;
@@ -142,9 +164,13 @@ const MakeAd = ({ jwt }) => {
             });
         }
         if (e.target.name === 'price') {
+            priceInputBox.error = false;
+            priceInputBox.errorMessage = '';
+            const value = e.target.value.replace(/[^0-9]/g, '');
+            validatePriceChange(value);
             setPriceInputBox({
                 ...priceInputBox,
-                value: e.target.value
+                value: value
             });
         }
     };
@@ -152,25 +178,71 @@ const MakeAd = ({ jwt }) => {
     const handleInputBoxBlur = (e) => {
         if (e.target.name === 'title') {
             validateTitleBlur(e.target.value);
+            setTitleInputBox({
+                ...titleInputBox,
+            });
+        }
+
+        if (e.target.name === 'price') {
+            validatePriceBlur(e.target.value);
+            setPriceInputBox({
+                ...priceInputBox,
+            });
         }
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         if (!jwt) {
             navigate('/login');
+            setIsError(true);
+            setErrorMessage('Please log in to submit an ad.');
             return;
         }
 
         validateTitleChange(titleInputBox.value);
         validateTitleBlur(titleInputBox.value);
         if (titleInputBox.error) {
+            console.log('Title error');
             return;
         }
+
+        validatePriceChange(priceInputBox.value);
+        validatePriceBlur(priceInputBox.value);
+        if (priceInputBox.error) {
+            console.log('Price error');
+            return;
+        }
+
         setUploadMedia(!uploadMedia);
+
+        try {
+            setIsError(false);
+            setErrorMessage('');
+            setIsSubmitting(true);
+
+            const headers = {
+                'Authorization': `Bearer ${jwt}`
+            }
+
+            const payload = {
+                title: titleInputBox.value,
+                price: priceInputBox.value,
+                period: period,
+                description: description
+            }
+
+            await axios.post(`${apiUrl}${uploadAssetEp}`, payload, { headers });
+
+        } catch (error) {
+            console.error('Error submitting ad:', error);
+            setIsError(true);
+            setErrorMessage('Error submitting ad. Please try again.');
+        }
     }
 
     return (
         <main className='make-ad'>
+            {isError && <div className='make-ad__error'>{errorMessage}</div>}
             <Section title='Title:' headingLevel='h2' isCollapsible={true} content={
                 <InputBox inputBoxData={titleInputBox} onChange={handleInputBoxChange} onBlur={handleInputBoxBlur} />
             } />
