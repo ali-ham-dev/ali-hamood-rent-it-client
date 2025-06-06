c238c5febb33386866b84230de030e1e1174125f
Ali Hamood <ali.hamood@gov.bc.ca>
2025-06-06 11:51:45 -0700

    Adds ManageRented page.

---
+++ src/pages/ManageRented/ManageRented.jsx
@@ -0,0 +1,11 @@
+import './ManageRented.scss';
+
+const ManageRented = ({ jwt }) => {
+    return (
+        <main className='manage-rented'>
+            <h1>Manage Rented</h1>
+        </main>
+    );
+};
+
+export default ManageRented;
